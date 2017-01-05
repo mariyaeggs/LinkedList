e90e94ec37278dc17e3858c4ad0bf1b09d9b94f3
mariyaeggs <mariyawilson@live.com>
2017-01-05 15:45:33 -0500

    Implement main function to generate user interaction and final linked list output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 /**
  * Inserting A Node At the Beginning of A Linked List
  * School: CSU, Monterey Bay
@@ -38,7 +39,7 @@ Node* Insert(Node* head, int x) {
  * @param args While temp != NULL
  * @return printf of data values in linked list
 */
-void Print() {
+void Print(Node* head) {
    struct Node* temp = head; // Local variable for a temporary node pointer
    printf("List is: ");
    while(temp != NULL){
@@ -47,7 +48,23 @@ void Print() {
    }
    printf("\n");
 }
+/**
+ * Main function generates linked list.
+ *
+ * @param args Count of data in list int n. Values comprising count, int x.
+ * @return
+*/
 int main() {
-
+   Node* head = NULL; // Create empty linked list
+   printf("How many numbers in the linked list?\n");
+   int i, n, x;
+   scanf("%d", &n);
+   // Iterate over count of linked list data
+   for(i=0; i<n; i++){
+      printf("Enter the number(s):\n");
+      scanf("%d", &x);
+      head = Insert(head,x);
+      Print(head);
+   }
     return 0;
 }
