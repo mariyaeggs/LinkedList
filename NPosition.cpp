93aba2ad2cddc5e2792dcfa97b33b8841b0a529c
mariyaeggs <mariyawilson@live.com>
2017-01-07 13:43:49 -0500

    Make several insertions in the main()

--- NPosition.cpp
+++ NPosition.cpp
@@ -16,6 +16,9 @@ struct Node {
     Node* next; // Address of consecutive node
 };
 struct Node* head; // Global variable for head pointer
+void Print();
+void Insert(int data, int n);
+
 /**
  * Main function computes node insertion
  * at nth position in the linked list.
@@ -25,4 +28,10 @@ struct Node* head; // Global variable for head pointer
 */
 int main() {
    head = NULL; // Empty linked list
+   Insert(2, 1); // List: 2
+   Insert(3, 2); // List : 3, 2
+   Insert(4, 1); // List : 4, 3, 2
+   Insert(5, 2); // List : 5, 4, 3, 2
+   Print();
+
 }
