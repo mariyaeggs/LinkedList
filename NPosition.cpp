30c09ca5927437122e244a7cde7898c7a3b97d36
mariyaeggs <mariyawilson@live.com>
2017-01-07 13:54:53 -0500

    Malloc 4 bytes of storage for a new node and point to data in Insert function

--- NPosition.cpp
+++ NPosition.cpp
@@ -7,21 +7,28 @@
  * Professor: Dr. Feiling Jia
  * Source: CLion 2016.3.2 on 1/5/2017
  *
- * Program inserts nodes at Nth position of a linked list.
+ * Program inserts nodes at Nth position in a linked list.
  *
  * @author Mariya Eggensperger
 */
 struct Node {
     int data;
-    Node* next; // Address of consecutive node
+    Node* next; // Address of consecutive node that links the list
 };
 struct Node* head; // Global variable for head pointer
 void Print();
-
+/**
+ * Function inserts specific nodes at
+ * nth position in the linked list.
+ *
+ * @param args int data, int n
+ * @return return void
+*/
 void Insert(int data, int n) {
-
+  Node* temp1 = new Node(); // Initiate malloc/new with 4 bytes of free storage from the Heap
+   temp1->data = data;
+   temp1->next = NULL;
 }
-
 /**
  * Main function computes node insertion
  * at nth position in the linked list.
