7979a7915aae35a8988e88ac9011e58777d42d2d
mariyaeggs <mariyawilson@live.com>
2017-01-08 11:43:39 -0500

    Implement Print() function

--- DeletePosition.cpp
+++ DeletePosition.cpp
@@ -25,23 +25,39 @@ int n; // Global variable for nth position
  * @return return void
 */
 void Insert(int data) {
-Node* temp1 = new Node(); // Initiate malloc/new with 4 bytes of free storage from the Heap
-temp1->data = data;
-temp1->next = NULL; // End linked list
-if(n==1) { // If the position of the node is equal to 1
-temp1->next=head;
-head = temp1;
-return;
+   Node* temp1 = new Node(); // Initiate malloc/new with 4 bytes of free storage from the Heap
+   temp1->data = data;
+   temp1->next = NULL; // End linked list
+   if(n==1) { // If the position of the node is equal to 1
+      temp1->next=head;
+      head = temp1;
+      return;
+   }
+   Node* temp2 = head; // Pointer for declaration of (n-1) position
+   for(int i=0; i<n-2; i++) { // Where i<n-2 assumes the (n-1) position
+      temp2 = temp2->next;
+   }
+   temp1->next = temp2->next;
+   temp2->next = temp1;
 }
-Node* temp2 = head; // Pointer for declaration of (n-1) position
-for(int i=0; i<n-2; i++) { // Where i<n-2 assumes the (n-1) position
-temp2 = temp2->next;
-}
-temp1->next = temp2->next;
-temp2->next = temp1;
+/**
+ * Function prints approved node(s)
+ * as a list at the program output.
+ *
+ * @param args While temp != NULL
+ * @return printf of data values in linked list
+*/
+
+void Print() {
+   struct Node* temp = head; // Local variable for a temporary node pointer
+   printf("List is:\n");
+   while(temp != NULL){
+      printf(" %d",temp->data);
+      temp = temp->next;
+   }
+   printf("\n\n");
 }
 
-void Print();
 void Delete(int n);
 /**
  * Main function tests node deletion
