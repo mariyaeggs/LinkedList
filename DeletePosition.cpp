c816e70b1f9eb9ec71b957630880448ecfe134c3
mariyaeggs <mariyawilson@live.com>
2017-01-08 13:00:10 -0500

    Change Insert function from Node* Insert(Node* head, int x) to Insert(int x) and modify code according

--- DeletePosition.cpp
+++ DeletePosition.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
  * Inserting A Node At the Beginning of A Linked List
  * Course: CST 370 Design & Analysis of Algorithms
@@ -25,21 +24,13 @@ int n; // Global variable for nth position
  * @param args int data, int n
  * @return return void
 */
-void Insert(int data) {
-   Node* temp1 = new Node(); // Initiate malloc/new with 4 bytes of free storage from the Heap
-   temp1->data = data;
-   temp1->next = NULL; // End linked list
-   if(n==1) { // If the position of the node is equal to 1
-      temp1->next=head;
-      head = temp1;
-      return;
-   }
-   Node* temp2 = head; // Pointer for declaration of (n-1) position
-   for(int i=0; i<n-2; i++) { // Where i<n-2 assumes the (n-1) position
-      temp2 = temp2->next;
-   }
-   temp1->next = temp2->next;
-   temp2->next = temp1;
+Node* Insert(int x) {
+   Node* temp = (Node*)malloc(sizeof(struct Node));
+   temp->data = x;
+   temp->next = NULL;
+   if(head != NULL) temp->next = head;
+   head = temp;
+   return head;
 }
 /**
  * Function prints nodes inserted
@@ -48,14 +39,14 @@ void Insert(int data) {
  * @param args While temp != NULL
  * @return printf of data values in linked list
 */
-void Print(Node* head) {
-   struct Node *temp = head; // Local variable for a temporary node pointer
-   printf("List is:\n");
-   while (temp != NULL) {
+void Print() {
+   Node* temp = head; // Initial temporary node points to the head
+   while(temp != NULL){
       printf(" %d", temp->data);
       temp = temp->next;
    }
-   printf("\n\n");
+   printf("\n");
+
 }
 /**
  * Function deletes node(s) at
---
+++ cmake-build-debug/CMakeFiles/DeletePosition.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeletePosition.cpp.dir/DeletePosition.cpp.o"
+  "DeletePosition.cpp.pdb"
+  "DeletePosition.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeletePosition.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
