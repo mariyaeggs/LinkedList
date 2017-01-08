e52aff1da776c0285bc7545cc14f9e99f7c39fe8
mariyaeggs <mariyawilson@live.com>
2017-01-08 13:02:36 -0500

    Clean up comments and code style for readability

--- DeletePosition.cpp
+++ DeletePosition.cpp
@@ -7,7 +7,7 @@
  * Professor: Dr. Feiling Jia
  * Source: CLion 2016.3.2 on 1/7/2017
  *
- * Program inserts nodes at the beginning of a linked list.
+ * Program deletes nodes at the beginning of a linked list.
  *
  * @author Mariya Eggensperger
 */
@@ -21,8 +21,8 @@ int n; // Global variable for nth position
  * Function inserts specific nodes at
  * nth position in the linked list.
  *
- * @param args int data, int n
- * @return return void
+ * @param args int n
+ * @return int x insertion
 */
 Node* Insert(int x) {
    Node* temp = (Node*)malloc(sizeof(struct Node));
@@ -30,13 +30,12 @@ Node* Insert(int x) {
    temp->next = NULL;
    if(head != NULL) temp->next = head;
    head = temp;
-   return head;
 }
 /**
  * Function prints nodes inserted
  * at nth position(s) in the list
  *
- * @param args While temp != NULL
+ * @param args while temp != NULL
  * @return printf of data values in linked list
 */
 void Print() {
@@ -52,17 +51,17 @@ void Print() {
  * Function deletes node(s) at
  * nth position(s) in the list
  *
- * @param args While temp != NULL
+ * @param args while temp != NULL
  * @return printf of data values in linked list
 */
 void Delete(int n) {
    Node * temp1 = head;
    if(n == 1) {
-       head = temp1->next;  // Head points to 2nd node
-       free(temp1);
+      head = temp1->next;  // Head points to 2nd node
+      free(temp1);
    }
    for(int i=0; i<n-2; i++) { // Where i<n-2 is the n-1th node
-       temp1 = temp1->next; // temp1 points to (n-1)th node
+      temp1 = temp1->next; // temp1 points to (n-1)th node
       Node* temp2 = temp1->next; // Nth node
       temp1->next = temp2->next; // (n+1)th Node
       free(temp2); // Delete temp2
